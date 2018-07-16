b001b90c596dda8ce28c47ebbbaae81c6fe41a23
Dingze <dingzew@andrew.cmu.edu>
2018-07-15 22:11:36 -0400

    modify style

--- hw4/hw4.rkt
+++ hw4/hw4.rkt
@@ -2,6 +2,10 @@
 
 (provide (all-defined-out))
 
+; author: Dingze Wang
+; UW CSE341 programming languages hw4
+
+; 1. concat everything from low to high in a list
 (define (sequence low hi stride)
   (letrec ([f (lambda (x)
               (if (<= x hi)
@@ -9,10 +13,13 @@
                   null))])
     (f low)))
 
+
+; 2. add a suffix after each string in the list
 (define (string-append-map xs suffix)
   (map (lambda (x) (string-append x suffix)) xs))
 
 
+; 3. get the n-th value in the list, if n > list.length, return (n % list.length)-th element
 (define (list-nth-mod xs n)
   (if (= (length xs) 0)
       (error "list-nth-mod: empty list")
@@ -21,6 +28,7 @@
             (car (list-tail xs (remainder n (length xs)))))))
          
 
+; 4. return the first n values in the stream as a list
 (define (stream-for-n-steps s n)
   (letrec ([f (lambda (stream count)
                 (if (< count n)
@@ -29,6 +37,7 @@
     (f s 0)))
 
 
+; 5. if the index % 5 == 0, then multiple it by -1
 (define funny-number-stream
   (letrec ([f (lambda (count)
                 (if (= 0 (remainder count 5))
@@ -37,21 +46,27 @@
            (lambda () (f 1))))
 
 
+; 6. return a stream show dan.jpg and dog.jpg back and force
 (define dan-then-dog
   (letrec ([dan (lambda () (cons "dan.jpg" dog))]
            [dog (lambda () (cons "dog.jpg" dan))])
     dan))
 
+
+; 7. return the stream that modify every element to a pair (0. original_value)
 (define (stream-add-zero s)
   (letrec ([f (lambda (stream)
                 (cons (cons 0 (car (stream))) (lambda () (f (cdr (stream))))))])
     (lambda () (f s))))
 
+
+; 8. combine elements in two lists as pair stream based on list-nth-mod
 (define (cycle-lists xs ys)
   (letrec ([f (lambda (count)
                 (cons (cons (list-nth-mod xs count) (list-nth-mod ys count)) (lambda () (f (+ 1 count)))))])
     (lambda () (f 0))))
 
+; 9. run assoc on a vector 
 (define (vector-assoc v vec)
   (letrec ([f (lambda (count)
                 (if (>= count (vector-length vec))
@@ -64,6 +79,7 @@
     (f 0)))
 
 
+; 10. use our own cache to run assoc before calling assoc
 (define (cached-assoc xs n)
   (letrec ([cache (make-vector n #f)]
     [index 0])
@@ -77,6 +93,7 @@
               #f)))))
 
 
+; challenge problem: if (e2) is less than (e1) then run e2 until (e2) is greater or equal than (e1)
 (define-syntax while-less
   (syntax-rules (do)
     [(while-less e1 do e2)
@@ -88,9 +105,3 @@
                           #t))])
        (loop))]))
        
-                                    
-
-           
-           
-               
-                
